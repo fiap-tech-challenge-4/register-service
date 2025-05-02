218e48d6f171bb71ee25830f0cada716bb483aca
Gustavo Biasi <>
2025-05-01 21:28:06 -0300

    removed descriptions and errors that aren't being used

--- src/main/java/br/com/register/webui/description/Descriptions.java
+++ src/main/java/br/com/register/webui/description/Descriptions.java
@@ -12,20 +12,12 @@ public class Descriptions {
     public static final String FIELD = "Name of the field with error";
     public static final String FIELD_MESSAGE = "Error message for the field";
 
-    public static final String RAZAO_SOCIAL = "Company's legal name";
-    public static final String NOME_FANTANSIA = "Trade name";
-    public static final String CNPJ = "CNPJ (Brazilian company ID)";
     public static final String ID = "Unique identifier";
-    public static final String ATIVO = "Active indicator. Allowed values: (S=YES / N=NO)";
     public static final String PAGE = "Page number";
     public static final String LIMIT = "Number of records per page";
     public static final String SORT = "Sorting order of the list";
     public static final String DESCRIPTION_PRODUCT = "Product description";
     public static final String DESCRIPTION_CATEGORY = "Category description";
-    public static final String ID_PRODUTO = "Unique identifier of the product";
-    public static final String ID_CLIENTE = "Unique identifier of the client";
-    public static final String ID_EMPRESA = "Unique identifier of the company";
-    public static final String OBSERVACAO = "Free-text field for order notes";
     public static final String NAME_CUSTOMER = "Client name";
     public static final String CPF_CUSTOMER = "Client CPF (Brazilian ID)";
     public static final String PHONE_CUSTOMER = "Phone number";
@@ -36,19 +28,6 @@ public class Descriptions {
     public static final String IMAGE_PRODUCT = "Image to display the product";
     public static final String DESCRIPTION_PAYMENT_METHOD = "Description of the payment method";
     public static final String PAYMENT_TYPE_PAYMENT_METHOD = "Type of payment method. Allowed values: [\"DEBITO\", \"CREDITO\", \"PIX\"]";
-    public static final String LISTA_ITENS_PEDIDO = "List of items in the order";
-    public static final String QTD_PRODUTO = "Quantity of products";
-    public static final String DATA_PEDIDO = "Date when the order was placed. Format: yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-    public static final String BLOCO_EMPRESA = "Company information block";
-    public static final String BLOCO_CLIENTE = "Client information block";
-    public static final String VLR_TOTAL = "Total order amount";
-    public static final String STATUS_PEDIDO = "Order status. Available values: RECEIVED, IN_PREPARATION, READY, COMPLETED";
-    public static final String PEDIDO_ID = "Unique identifier of the order";
-    public static final String FORMA_PAGAMENTO_ID = "Unique identifier of the payment method";
-    public static final String QRCODE = "Payment QRCode";
-    public static final String IDENTIFICADOR_PAGAMENTO = "Unique payment identifier";
-    public static final String VLR_PAGAMENTO = "Amount of the payment made";
-    public static final String STATUS_PAGAMENTO = "Payment status. Available values: PENDING, CONFIRMED, DECLINED";
 
     // Pagination
     public static final String HAS_NEXT = "Indicates if there is a next page with elements";

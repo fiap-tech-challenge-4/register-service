d7888ee551b47b0b42f2755f96d593289eb463ca
Gustavo Biasi <>
2025-05-05 19:08:02 -0300

    add sonar

--- src/main/java/br/com/register/webui/mappers/CategoryMapper.java
+++ src/main/java/br/com/register/webui/mappers/CategoryMapper.java
@@ -2,8 +2,8 @@ package br.com.register.webui.mappers;
 
 import br.com.register.app.entities.Category;
 import br.com.register.webui.dtos.CategoryDTO;
-import br.com.register.webui.dtos.request.UpdateCategoryRequest;
 import br.com.register.webui.dtos.request.RegisterCategoryRequest;
+import br.com.register.webui.dtos.request.UpdateCategoryRequest;
 import br.com.register.webui.dtos.response.CategoryResponse;
 import org.mapstruct.Mapper;
 
